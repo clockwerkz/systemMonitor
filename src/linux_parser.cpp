54c24d8f7e341d1ab9fe2d49757d838291369bef
Carlos <clockwerkz@gmail.com>
2020-12-14 11:44:02 -0800

    figuring out system jiffies

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -104,7 +104,22 @@ long LinuxParser::UpTime() {
 }
 
 // TODO: Read and return the number of jiffies for the system
-long LinuxParser::Jiffies() { return 0; }
+long LinuxParser::Jiffies() { 
+  long totalJiffies;
+  std::ifstream file(kProcDirectory + kStatFilename);
+  if (file.is_open()) {
+    string line;
+    while (std::getline(file, line)) {
+      std::istringstream linestream(line);
+      string cpu, normal, nice, system, idle;
+      linestream >> cpu >> normal >> nice >> system >> idle;
+      if (cpu == "cpu") {
+        return (stol(normal) + stol(nice) + stol(system) + stol(idle));
+      }
+    }
+  }
+  return totalJiffies;
+}
 
 // TODO: Read and return the number of active jiffies for a PID
 // REMOVE: [[maybe_unused]] once you define the function
--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,29 @@ int main() {
   System system;
   //Testing Parsing of files:
   srand((unsigned) time(0));
+  cout << "Operating System" << std::endl;
+  cout << "----------------" << std::endl;
   cout << LinuxParser::OperatingSystem() << std::endl;
   cout << LinuxParser::Kernel() << std::endl;
+  long totalJiffies = LinuxParser::Jiffies();
+  cout << "Number of jiffies for the system: " << std::to_string(totalJiffies) << std::endl;
   LinuxParser::MemoryUtilization(); 
   std::vector<int> pids;
   pids = LinuxParser::Pids();
   int index = rand() % pids.size();
   int uid = std::stoi(LinuxParser::Uid(pids[index]));
   std::string userId = LinuxParser::User(uid);
+  cout << std::endl;
+  cout << "Individual PID" << std::endl;
+  cout << "--------------" << std::endl;
   cout << "User of random pid (" << uid <<") in list (" << pids[index] << "): " << userId << std::endl;
   cout << "Command line: " << LinuxParser::Command(pids[index]) << std::endl;
   cout << "Uptime in seconds: " << LinuxParser::UpTime() << std::endl;
-  cout << "Time format test for 22:30:45 " << Format::ElapsedTime(81045) << std::endl;
-  cout << "Time format test for 05:09:30 " << Format::ElapsedTime(18570) << std::endl;
+  cout << std::endl;
+  cout << "Format Time" << std::endl;
+  cout << "-----------" << std::endl;
+  cout << "Time format test for 22:30:45- " << Format::ElapsedTime(81045) << std::endl;
+  cout << "Time format test for 05:09:30- " << Format::ElapsedTime(18570) << std::endl;
+  cout << "Time format test for 00:00:30- " << Format::ElapsedTime(30) << std::endl;
   //NCursesDisplay::Display(system);
 }
